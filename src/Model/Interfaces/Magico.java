4a5d49a8efe567b67bf570b79fafc042277dcab4
JorgeVeres <moganero292@gmail.com>
2025-05-01 14:57:26 +0100

    Fix package naming conventions in interface imports

--- src/Model/Interfaces/Magico.java
+++ src/Model/Interfaces/Magico.java
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-package model.Interfaces;
-=======
 package model.interfaces;
->>>>>>> 4bc15f702add6105ed89cbef0ce2fa86a4e9c0f9
 
 public interface Magico {
     double lanzarHechizo();
--- src/Model/Interfaces/Volador.java
+++ src/Model/Interfaces/Volador.java
@@ -1,4 +1,4 @@
-package model.Interfaces;
+package model.interfaces;
 
 public interface Volador {
     void volar();
